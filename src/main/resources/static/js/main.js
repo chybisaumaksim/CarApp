48c5484e4ba1953496964c3e2623b4100538bc73
m.chybisau@godeltech.com <maksim.chybisau@gmail.com>
2019-07-23 13:29:13 +0300

    feature/L18-jQuery : First changes

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,10 @@
-// The root URL for the RESTful services
 var rootURL = "http://localhost:8080/pt11/cars";
 
-var currentWine;
+var currentCar;
 
 findAll();
 
-// Nothing to delete in initial application state
-$('#btnDelete').hide();
+// $('#btnDelete').hide();
 
 // Register listeners
 $('#btnSearch').click(function () {
@@ -14,54 +12,60 @@ $('#btnSearch').click(function () {
     return false;
 });
 
-// Trigger search when pressing 'Return' on search key input field
-$('#searchKey').keypress(function (e) {
-    if (e.which == 13) {
-        search($('#searchKey').val());
-        e.preventDefault();
-        return false;
-    }
-});
+// // Trigger search when pressing 'Return' on search key input field
+// $('#searchKey').keypress(function (e) {
+//     if (e.which == 13) {
+//         search($('#searchKey').val());
+//         e.preventDefault();
+//         return false;
+//     }
+// });
 
 $('#btnAdd').click(function () {
-    newWine();
+    newCar();
     return false;
 });
 
 $('#btnSave').click(function () {
-    if ($('#id').val() == '')
-        addWine();
-    else
-        updateWine();
+    addCar();
+    findAll();
     return false;
 });
+//
+// $('#btnSave').click(function () {
+//     if ($('#id').val() == '')
+//         addCar();
+//     else
+//         updateWine();
+//     return false;
+// });
 
-$('#btnDelete').click(function () {
-    deleteWine();
-    return false;
-});
 
-$('#wineList a').live('click', function () {
-    findById($(this).data('identity'));
-});
-
-// Replace broken images with generic wine bottle
-$("img").error(function () {
-    $(this).attr("src", "pics/generic.jpg");
-
-});
-
-function search(searchKey) {
-    if (searchKey == '')
-        findAll();
-    else
-        findByName(searchKey);
+function btnDelete(id) {
+    $('#btnDelete').click(function () {
+        deleteCar(id);
+        return false;
+    });
 }
 
-function newWine() {
-    $('#btnDelete').hide();
-    currentWine = {};
-    renderDetails(currentWine); // Display empty form
+$('#btnUpdate').click(function () {
+    updateCar();
+    return false;
+});
+
+// $('#carList a').live('click', function () {
+//     removeCar(a);
+// });
+
+
+function search(colour) {
+    findByColour(colour);
+    return false;
+}
+
+function newCar() {
+    currentCar = {};
+    renderDetails(currentCar);
 }
 
 function findAll() {
@@ -69,37 +73,22 @@ function findAll() {
     $.ajax({
         type: 'GET',
         url: rootURL,
-        dataType: "json", // data type of response
-        success: renderList
-    });
-}
-
-function findByName(searchKey) {
-    console.log('findByName: ' + searchKey);
-    $.ajax({
-        type: 'GET',
-        url: rootURL + '/search/' + searchKey,
         dataType: "json",
         success: renderList
     });
 }
 
-function findById(id) {
-    console.log('findById: ' + id);
+function findByColour(colour) {
+    console.log('findByColour: ' + colour);
     $.ajax({
         type: 'GET',
-        url: rootURL + '/' + id,
+        url: rootURL + '/byColour/' + colour,
         dataType: "json",
-        success: function (data) {
-            $('#btnDelete').show();
-            console.log('findById success: ' + data.name);
-            currentWine = data;
-            renderDetails(currentWine);
-        }
+        success: renderList
     });
 }
 
-function addWine() {
+function addCar() {
     console.log('addCar');
     $.ajax({
         type: 'POST',
@@ -107,39 +96,60 @@ function addWine() {
         url: rootURL,
         dataType: "json",
         data: formToJSON(),
-        success: function (data, textStatus, jqXHR) {
-            alert('Car created successfully');
-            $('#btnDelete').show();
-            $('#wineId').val(data.id);
-        },
+        success:
+            function (car, textStatus, jqXHR) {
+                $('#cars_table_body').append(
+                    '<tr>' +
+                    '<td>' + car.id + '</td>' +
+                    '<td>' + car.model + '</td>' +
+                    '<td>' + car.colour + '</td>' +
+                    '<td style="width: 100px"><button class=\'btn btn-primary \' onclick="removeCar(' + car.id + ', this)">Remove</button></td>' +
+                    '<td style="width: 100px"><button class=\'btn btn-primary \' onclick="updateCar(' + car.id + ')">Update</button></td>' +
+                    '</tr>');
+                renderList;
+            },
         error: function (jqXHR, textStatus, errorThrown) {
-            alert('addWine error: ' + textStatus);
+            alert('addCar: ' + textStatus);
         }
     });
 }
 
-function updateWine() {
+function updateCar() {
     console.log('updateCar');
     $.ajax({
         type: 'PUT',
         contentType: 'application/json',
-        url: rootURL + '/' + $('#wineId').val(),
+        url: rootURL + '/' + $('#id').val(),
         dataType: "json",
         data: formToJSON(),
         success: function (data, textStatus, jqXHR) {
-            alert('Car updated successfully');
+            $(th).closest('tr').hide();
         },
         error: function (jqXHR, textStatus, errorThrown) {
-            alert('updateCar error: ' + textStatus);
+            alert('updateCar: ' + textStatus);
         }
     });
 }
 
-function deleteWine() {
-    console.log('deleteWine');
+// function deleteCar(id) {
+//     console.log('deleteCar');
+//     $.ajax({
+//         type: 'DELETE',
+//         url: rootURL + '/' + $('#id').val(),
+//         url: rootURL + '/' + id,
+//         success: function (data, textStatus, jqXHR) {
+//         },
+//         error: function (jqXHR, textStatus, errorThrown) {
+//             alert('deleteCar: ' + textStatus);
+//         }
+//     });
+// }
+
+function deleteCar() {
+    console.log('deleteCar');
     $.ajax({
         type: 'DELETE',
-        url: rootURL + '/' + $('#wineId').val(),
+        url: rootURL + '/' + $('#id').val(),
         success: function (data, textStatus, jqXHR) {
             alert('Wine deleted successfully');
         },
@@ -149,15 +159,27 @@ function deleteWine() {
     });
 }
 
-function renderList(data) {
-    // JAX-RS serializes an empty list as null, and a 'collection of one' as an object (not an 'array of one')
-    var list = data == null ? [] : (data instanceof Array ? data : [data]);
-
-    $('#wineList li').remove();
-    $.each(list, function (index, car) {
-        $('#wineList').append('<li><a href="#" data-identity="' + car.id + '">' + car.model + ' ' + car.colour + '</a></li>');
+function removeCar(id, th) {
+    $.ajax({
+        type: 'DELETE',
+        url: rootURL + '/' + $('#id').val(),
+        success: function (data, textStatus, jqXHR) {
+            $(th).closest('tr').hide();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('deleteWine error');
+        }
     });
 }
+
+// function renderList(cars) {
+//     var list = cars == null ? [] : (cars instanceof Array ? cars : [cars]);
+//     $('#carList li').remove();
+//     $.each(list, function (index, car) {
+//         $('#carList').append('<li><a href="#" cars-identity="' + car.id + '">' + car.model + ' ' + car.colour + '<input type="button" value="Delete Row" onclick="SomeDeleteRowFunction(this)"></li></a>');
+//         // $('#carList').append('<input type="button" value="Delete Row" onclick="SomeDeleteRowFunction(this)"></li>');
+//     });
+// }
 
 function renderDetails(car) {
     $('#id').val(car.id);
@@ -165,7 +187,6 @@ function renderDetails(car) {
     $('#colour').val(car.colour);
 }
 
-// Helper function to serialize all the form fields into a JSON string
 function formToJSON() {
     var id = $('#id').val();
     return JSON.stringify({
@@ -173,4 +194,26 @@ function formToJSON() {
         "model": $('#model').val(),
         "colour": $('#colour').val()
     });
+}
+
+function renderList(cars) {
+    var tableBody = $('#cars_table_body');
+    tableBody.hide();
+    tableBody.empty();
+    if (cars && cars.length) {
+        cars.forEach(function (car) {
+            tableBody.append(
+                '<tr>' +
+                '<td>' + car.id + '</td>' +
+                '<td>' + car.model + '</td>' +
+                '<td>' + car.colour + '</td>' +
+                '<td style="width: 100px"><button class=\'btn btn-primary \' onclick="removeCar(' + car.id + ', this)">Remove</button></td>' +
+                '<td style="width: 100px"><button class=\'btn btn-primary \' onclick="updateCar(' + car.id + ', this)">Update</button></td>' +
+                '</tr>')
+        });
+    } else {
+        tableBody.append('<tr><td colspan="4">No matching records found</td></tr>')
+    }
+
+    tableBody.show();
 }
